1b6067c263b47e764599ffc90f18665be58c9972
Pimpinee Jatuwongwiwat <Pimpinee@tencent.co.th>
2017-11-10 17:18:30 +0700

    https://browsersync.io/docs/gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 gulp.task('compile', function () {
 	'use strict';
 	var twig = require('gulp-twig');
-	return gulp.src('src/views/index.twig')
+	return gulp.src('src/views/*.twig')
 			.pipe(twig({
 					data: {
 							title: 'Gulp and Twig',
@@ -17,6 +17,21 @@ gulp.task('compile', function () {
 			.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['compile'], function() {
-	gulp.watch("src/views/index.twig", ['compile']);
+/* ส่วนของ browserSync */
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+	 browserSync({
+			 server: {
+					 baseDir: "dist/"
+			 }
+	 });
+});
+
+gulp.task('default', ['compile', 'browser-sync'], function() {
+	gulp.watch("src/views/*.twig", ['compile']);
+
+	// เมื่อไฟล์ html มีการเปลี่ยนแปลง ก็ให้รีเฟรช web browser
+	gulp.watch(['dist/*.html'], browserSync.reload);
+
 });
